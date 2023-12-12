09466653d907abad2295326d3661020acb6645d7
Uzgar <100992320+Uzgar@users.noreply.github.com>
2023-12-12 22:47:06 +0100

    added chat thing with image loader (test)
    yes.

---
+++ reboot/IMG_client.py
@@ -0,0 +1,133 @@
+import tkinter as tk
+from tkinter import filedialog
+from socket import socket, AF_INET, SOCK_STREAM
+from threading import Thread
+import os
+
+class UsernameDialog(tk.Toplevel):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.title("Enter Username")
+        self.geometry("300x100")
+
+        self.label = tk.Label(self, text="Choose a username:")
+        self.label.pack(pady=10)
+
+        self.entry_var = tk.StringVar()
+        self.entry = tk.Entry(self, textvariable=self.entry_var, width=20)
+        self.entry.pack(pady=10)
+
+        self.ok_button = tk.Button(self, text="OK", command=self.ok_button_click)
+        self.ok_button.pack()
+
+    def ok_button_click(self):
+        self.username = self.entry_var.get()
+        self.destroy()
+
+class ClientGUI:
+    def __init__(self, master):
+        self.master = master
+        self.master.title("Chat Client")
+
+        self.message_listbox = tk.Listbox(self.master, height=15, width=50)
+        self.message_listbox.pack(padx=10, pady=10)
+
+        self.entry_var = tk.StringVar()
+        self.entry = tk.Entry(self.master, textvariable=self.entry_var, width=40)
+        self.entry.pack(padx=10, pady=10)
+
+        self.browse_button = tk.Button(self.master, text="Browse", command=self.browse_image)
+        self.browse_button.pack(pady=10)
+
+        self.send_button = tk.Button(self.master, text="Send", command=self.send_message)
+        self.send_button.pack(pady=10)
+
+        self.users_label = tk.Label(self.master, text="Connected Users: 0")
+        self.users_label.pack(side=tk.BOTTOM, padx=10, pady=10)
+
+        self.username_dialog = UsernameDialog(self.master)
+        self.master.wait_window(self.username_dialog)
+
+        self.username = self.username_dialog.username
+        self.sock = socket(AF_INET, SOCK_STREAM)
+        self.server_address = ('192.168.137.145', 5566)  # Replace SERVER_IP with the actual IP address of your server
+        self.sock.connect(self.server_address)
+
+        # Send the chosen username to the server
+        self.sock.sendall(self.username.encode('utf-8'))
+
+        # Start the thread for receiving messages and user count
+        receive_thread = Thread(target=self.receive_messages)
+        receive_thread.start()
+
+    def browse_image(self):
+        file_path = filedialog.askopenfilename(filetypes=[("Image files", "*.png;*.jpg;*.jpeg;*.gif")])
+        self.entry_var.set(f"/send_image {file_path}")
+
+    def send_message(self):
+        message = self.entry_var.get()
+        if message.startswith("/send_image"):
+            self.send_image(message)
+        else:
+            self.sock.sendall(message.encode('utf-8'))
+            self.message_listbox.insert(tk.END, f"{self.username}: {message}")
+        self.entry_var.set("")
+
+    def send_image(self, message):
+        # Extract the path from the command
+        path = message.split(" ")[1]
+        try:
+            with open(path, 'rb') as file:
+                image_data = file.read()
+            filename = os.path.basename(path)
+            self.sock.sendall(f"/send_image {filename}".encode('utf-8'))
+            self.sock.sendall(image_data)
+            self.message_listbox.insert(tk.END, f"{self.username} sent an image: {filename}")
+        except FileNotFoundError:
+            self.message_listbox.insert(tk.END, f"Image file not found: {path}")
+
+    def receive_messages(self):
+            while True:
+                try:
+                    data = self.sock.recv(1024)
+                    if not data:
+                        break
+                    message = data.decode('utf-8')
+                    if message.startswith("USER_COUNT:"):
+                        parts = message.split(":")
+                        if len(parts) == 2:
+                            try:
+                                count = int(parts[1])
+                                self.update_user_count(count)
+                                continue  # Skip the rest of the loop for user count messages
+                            except ValueError:
+                                print("Invalid USER_COUNT message format:", message)
+                                continue
+                    elif message.startswith("/send_image"):
+                        self.receive_image()
+                    else:
+                        self.message_listbox.insert(tk.END, message)
+                except ConnectionError:
+                    break
+
+
+    def receive_image(self):
+        try:
+            filename = self.sock.recv(1024).decode('utf-8')
+            image_data = self.sock.recv(1024)
+            with open(filename, 'wb') as file:
+                file.write(image_data)
+            self.message_listbox.insert(tk.END, f"{self.username} sent an image: {filename}")
+        except Exception as e:
+            print(f"Error receiving image: {e}")
+
+    def update_user_count(self, count):
+        self.users_label.config(text=f"Connected Users: {count}")
+
+def main():
+    root = tk.Tk()
+    client_gui = ClientGUI(root)
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
---
+++ reboot/IMG_server.py
@@ -0,0 +1,97 @@
+from socket import socket, AF_INET, SOCK_STREAM
+from threading import Thread
+
+class Server:
+    def __init__(self):
+        self.sock = socket(AF_INET, SOCK_STREAM)
+        self.server_address = ('192.168.137.145', 5566)
+        self.sock.bind(self.server_address)
+        self.sock.listen(5)
+        print("Server listening on", self.server_address)
+
+        self.clients = {}  # Dictionary to store clients and their usernames
+
+    def start(self):
+        user_count_thread = Thread(target=self.send_user_count)
+        user_count_thread.start()
+
+        while True:
+            client_sock, client_address = self.sock.accept()
+            print("Connection from", client_address)
+
+            # Receive the username from the client
+            username = client_sock.recv(1024).decode('utf-8')
+            print(f"{client_address} chose the username: {username}")
+
+            self.clients[client_sock] = username
+
+            # Broadcast the user count to all clients
+            self.broadcast_user_count()
+
+            client_thread = Thread(target=self.handle_client, args=(client_sock,))
+            client_thread.start()
+
+    def handle_client(self, client_sock):
+        while True:
+            try:
+                data = client_sock.recv(1024)
+                if not data:
+                    break
+                message = data.decode('utf-8')
+                sender_username = self.clients[client_sock]
+
+                if message.startswith("/send_image"):
+                    self.handle_image_message(client_sock)
+                else:
+                    # Broadcast the message to all clients with sender's username
+                    for client, username in list(self.clients.items()):
+                        if client != client_sock:
+                            client.sendall(f"{sender_username}: {message}".encode('utf-8'))
+            except ConnectionError:
+                break
+
+        # Remove the client from the dictionary and broadcast the updated user count
+        del self.clients[client_sock]
+        self.broadcast_user_count()
+
+    def handle_image_message(self, client_sock):
+        try:
+            filename = client_sock.recv(1024).decode('utf-8')
+            image_data = client_sock.recv(1024)
+            with open(filename, 'wb') as file:
+                file.write(image_data)
+
+            # Broadcast the image message to all clients
+            for client in list(self.clients):
+                try:
+                    client.sendall(f"{self.clients[client_sock]} sent an image: {filename}".encode('utf-8'))
+                except ConnectionError:
+                    continue
+        except Exception as e:
+            print(f"Error handling image message: {e}")
+
+    def send_user_count(self):
+        while True:
+            user_count = len(self.clients)
+            # Broadcast the user count to all clients
+            self.broadcast_user_count(user_count)
+
+    def broadcast_user_count(self, count=None):
+        if count is None:
+            count = len(self.clients)
+
+        # Créez une copie du dictionnaire avant l'itération
+        clients_copy = dict(self.clients)
+
+        for client, _ in clients_copy.items():
+            try:
+                client.sendall(f"USER_COUNT:{count}".encode('utf-8'))
+            except ConnectionError:
+                continue
+
+def main():
+    server = Server()
+    server.start()
+
+if __name__ == "__main__":
+    main()
